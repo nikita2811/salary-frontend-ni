d80ef63271a5a1a86b5458cf4ca49841eee869a7
nikita2811 <39294680+nikita2811@users.noreply.github.com>
2026-05-19 11:12:25 +0530

    bug fix : bug fix for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-import CreateEmployee from './components/createEmployee'
+import CreateEmployee from './components/CreateEmployee'
 import Home from './components/Home'
 import ViewEmployee from './components/ViewEmployee'
 import UpdateEmployee from './components/UpdateEmployee'
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const App: React.FC = () => {
 
